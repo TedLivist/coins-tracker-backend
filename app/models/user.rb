b6d1b0e7a25fc155f7e39621dd035ad9de06c1fd
TedLivist <livistteddy@gmail.com>
2022-02-27 02:15:22 +0100

    Fix uniquness of username issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 
   validates :username, presence: true,
             length: {minimum: 3, maximum: 10},
-            uniqueness: true
+            uniqueness: { case_sensitive: false }
 end
