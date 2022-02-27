8f755ab0c3a0611f0cebbf994eea8f2147db304f
TedLivist <livistteddy@gmail.com>
2022-02-27 02:11:42 +0100

    Add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-  
+
+  before_save { self.username = username.downcase }
+
+  validates :username, presence: true,
+            length: {minimum: 3, maximum: 10},
+            uniqueness: true
 end
